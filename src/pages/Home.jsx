36d4e8c7f388be0017dea4423b23caf5272a0e95
ahsfar <ahsfar21@gmail.com>
2022-04-09 20:58:50 +0800

    github-api-users-react

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+
+function Home() {
+  return (
+    <>
+      <UserResults/>
+    </>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ['dark'],
+},
+};
